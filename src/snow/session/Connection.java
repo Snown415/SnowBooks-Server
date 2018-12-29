1f9ef5a4dd5943cd3c68a9d2870b3665d9a00d43
Nathaniel Snow <Snown415@icloud.com>
2018-12-29 12:30:57 -0500

    Added scheduler

--- src/snow/session/Connection.java
+++ src/snow/session/Connection.java
@@ -2,6 +2,9 @@ package snow.session;
 
 import java.net.Socket;
 import java.util.LinkedHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -17,10 +20,12 @@ import snow.packet.PacketHandler;
 public class Connection {
 	
 	private static @Getter LinkedHashMap<String, Connection> connections = new LinkedHashMap<>();
+	private static final @Getter ScheduledExecutorService SCHEDULER = Executors.newScheduledThreadPool(Integer.MAX_VALUE); 
 
 	private @Getter @Setter Socket socket;
 	private @Getter @Setter Worker worker;
 	private @Getter @Setter PacketHandler packetHandler;
+	private @Getter @Setter ScheduledExecutorService requester;
 	
 	public Connection(Socket socket, Worker worker) {
 		setSocket(socket);
@@ -30,6 +35,19 @@ public class Connection {
 		
 		String ip = socket.getInetAddress().getHostAddress();
 		System.out.println("A connection to " + ip + " has been made.");
+		startSession();
+	}
+	
+	private void startSession() {
+		SCHEDULER.scheduleAtFixedRate(new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("Handling Session for " + socket.getInetAddress().getHostAddress());
+				
+			}
+			
+		}, 0, 1, TimeUnit.SECONDS);
 	}
 	
 }
